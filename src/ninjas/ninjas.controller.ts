ca183db45164cda3e15130a5d3ae9e2330d6913e
Roshan Sebastian <roshan.es@confianzit.com>
2023-03-19 23:21:27 +0530

    Learned param handling and body handling

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+  // to get all ninjas
+  @Get()
+  getNinjas(@Query('type') type: string) {
+    if (type == 'fast') {
+      return ['You are asking for a fast ninja'];
+    } else {
+      return ['You are asking for a slow ninja'];
+    }
+  }
+
+  // to get a particular ninja
+  @Get(':id')
+  getNinjaFromId(@Param('id') id: string) {
+    return { id };
+  }
+
+  // to add a ninja
+  @Post()
+  createANinja(@Body() createNinjaDto: CreateNinjaDto) {
+    return { name: createNinjaDto.name };
+  }
+
+  // to edit a ninja
+  @Put(':id')
+  updateNinja() {
+    return {};
+  }
+
+  // to delete a ninja
+  @Delete(':id')
+  deleteNinja() {
+    return {};
+  }
+}
